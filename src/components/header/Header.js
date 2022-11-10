14429d64014efc5aa332b2cd2a56b0358c56873f
JK1320 <kaurjus1@gmail.com>
2022-11-10 12:49:59 +0000

    Import createTheme & ThemeProvider from MUI

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,29 @@
+// themeProvider allows us to apply themes to MUI components
+import { createTheme, TextField, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import "./Header.css";
 
 const Header = () => {
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#fff'
+    },
+    type: "dark",
+  },
+});
+
   return (
-    <div className='header'>
-        <span className='title'>Word Hunt</span>
+    <div className="header">
+      <span className="title">Word Hunt</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+        </ThemeProvider>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
