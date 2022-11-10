688fb6fd1ef4fa7f6afc135da78cd4bfdc1069f5
JK1320 <kaurjus1@gmail.com>
2022-11-10 23:32:37 +0000

    Add ternary Operator to display text

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 import "./Header.css";
 import categories from "../../data/category";
 
-const Header = ({category, setCategory}) => {
+const Header = ({category, setCategory, word, setWord}) => {
 
 const darkTheme = createTheme({
   palette: {
@@ -17,16 +17,19 @@ const darkTheme = createTheme({
 
   return (
     <div className="header">
-      <span className="title">Word Hunt</span>
+      <span className="title">{word ? word : "Word Hunt"}</span>
       <div className="inputs">
         <ThemeProvider theme={darkTheme}>
           <TextField
-          className='search'
+            className="search"
             label="Search a word"
             variant="standard"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
           />
           <TextField
             select
+            className="select"
             label="Language"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
