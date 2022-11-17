b0534a3c8389f236f5a3f8815ae2f72f486d646b
JK1320 <kaurjus1@gmail.com>
2022-11-17 22:49:07 +0000

    Add audio tag

--- src/components/definitions/Definitions.js
+++ src/components/definitions/Definitions.js
@@ -3,32 +3,43 @@ import "./Definitions.css";
 
 const Definitions = ({word, category, meanings}) => {
 
-    
+    console.log(meanings)
   return (
-    <div className='meanings'>
-        {word === "" ? <span className='subTitle'>Start by typing a word in Search</span>:
-        (
-            meanings.map((mean) => mean.meanings.map((item) => 
-                item.definitions.map((def) => (
-                    <div 
-                    className='singleMean' 
-                    style={{backgroundColor: "white", color: "black"}}
-                    >
-                        <b>{def.definition}</b>
-                        <hr style={{backgroundColor: "black", width: "100%"}} />
-                        {def.example && (
-                                <span>
-                                    <b>Example: </b>
-                                    {def.example}    
-                                </span>
-                            )}
-                    </div>
-                ))
+    <div className="meanings">
+      {meanings[0] && word && category === "en" && (
+        <audio
+          src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
+          style={{ backgroundColor: "#fff", borderRadius: 10 }}
+          controls
+        >
+          Your browser does not support audio element.
+        </audio>
+      )}
+      {word === "" ? (
+        <span className="subTitle">Start by typing a word in Search</span>
+      ) : (
+        meanings.map((mean) =>
+          mean.meanings.map((item) =>
+            item.definitions.map((def) => (
+              <div
+                className="singleMean"
+                style={{ backgroundColor: "white", color: "black" }}
+              >
+                <b>{def.definition}</b>
+                <hr style={{ backgroundColor: "black", width: "100%" }} />
+                {def.example && (
+                  <span>
+                    <b>Example: </b>
+                    {def.example}
+                  </span>
+                )}
+              </div>
             ))
+          )
         )
-        }
+      )}
     </div>
-  )
+  );
 }
 
 export default Definitions;
